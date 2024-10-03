8f133704050f5f760cfe9b8583cf2c27847e8171
MarinaAlber <marina.alber@outlook.com>
2024-10-04 01:20:44 +0300

    chore: adding settings

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  sassOptions: {
+    additionalData: `@import "src/theme/main.scss";`,
+  },
+  experimental: {
+    optimizePackageImports: ["@mantine/core", "@mantine/hooks"],
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      use: ["@svgr/webpack"],
+    });
+    return config;
+  },
+};
 
 export default nextConfig;
